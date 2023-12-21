5da61e55d117376a418c87720795c91b2f18e915
Gabriel Sirvent <117134089+Dokjahyung@users.noreply.github.com>
2023-12-21 15:48:39 -0800

    Delete Portfolio directory

--- Portfolio/email.js
+++
@@ -1,4 +0,0 @@
-function toggleForm() {
-    const form = document.querySelector('.form-container');
-    form.style.display = form.style.display === 'none' ? 'block' : 'none';
-}
--- Portfolio/script.js
+++
@@ -1,9 +0,0 @@
-function openMenu() {
-    var menu = document.querySelector('.menu');
-    menu.style.display = 'flex';
-}
-
-function closeMenu() {
-    var menu = document.querySelector('.menu');
-    menu.style.display = 'none';
-}
