f85a2eb3093d3bbf51865954afcd64df726f3822
Gabriel Sirvent <117134089+Dokjahyung@users.noreply.github.com>
2023-12-21 15:43:20 -0800

    starter portfolio

---
+++ Portfolio/script.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    var menu = document.querySelector('.menu');
+    menu.style.display = 'flex';
+}
+
+function closeMenu() {
+    var menu = document.querySelector('.menu');
+    menu.style.display = 'none';
+}
